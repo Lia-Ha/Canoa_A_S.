f91759b4104831a562376a56daef2c61fa10a30d
Ojuelas <165110788+Lia-Ha@users.noreply.github.com>
2024-10-17 21:58:11 -0500

    Update Canoa.A.py

--- Canoa_Amazonica_BOT/Canoa.A.py
+++ Canoa_Amazonica_BOT/Canoa.A.py
@@ -51,15 +51,15 @@ st.markdown(
 # Agregar el div del superpuesto en la parte superior
 st.markdown("<div class='overlay'></div>", unsafe_allow_html=True)
 
-# URLs de las imágenes
-url_chica_comida = "https://raw.githubusercontent.com/Lia-Ha/Canoa_A_S./main/Canoa_Amazonica_BOT/La%20Canooa.jpg"
-
-# Mostrar imágenes en la barra lateral
-st.sidebar.image(url_chica_comida, caption="Deliciosos Manjares de la Selva", use_column_width=True)
-
 # Menú lateral
 menu = ["La Canoa Amazónica", "Ofertas", "Pedidos", "Reclamos"]
 choice = st.sidebar.selectbox("Menú", menu)
+
+# URLs de las imágenes
+url_chica_comida = "https://raw.githubusercontent.com/Lia-Ha/Canoa_A_S./main/Canoa_Amazonica_BOT/La%20Canooa.jpg"
+
+# Mostrar imágenes en la barra lateral debajo del menú
+st.sidebar.image(url_chica_comida, caption="Deliciosos Manjares de la Selva", use_column_width=True)
 
 if choice == "La Canoa Amazónica":
     # Mensaje de bienvenida en HTML con estilo
